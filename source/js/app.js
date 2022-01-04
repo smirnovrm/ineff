00b45413e4f820da7e5fbe1efda10ab286960996
smirnovrm <r.smirnov86@gmail.com>
2022-01-04 17:09:07 +0600

    добавляет разметку для блока с примером отчета

--- source/js/app.js
+++ source/js/app.js
@@ -4,3 +4,20 @@ const mainMenu = document.querySelector('.main-menu__list');
 menuButton.addEventListener('click', (event) => {
   mainMenu.classList.toggle('main-menu__list--opened')
 });
+
+const tabs = document.querySelectorAll('.tabs__control');
+const contents = document.querySelectorAll('.tabs__content');
+
+tabs.forEach((tab, index, parent) => {
+  tab.addEventListener('click', (e) => {
+    e.preventDefault();
+    parent.forEach(item => {
+      item.classList.remove('tabs__control--active');
+    });
+    contents.forEach(item => {
+      item.classList.remove('tabs__content--active');
+    });
+    tab.classList.add('tabs__control--active');
+    contents[index].classList.add('tabs__content--active');
+  });
+})
