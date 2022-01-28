629e379f2ecef7ee3de8cfeac0a26fdbb71c987a
smirnovrm <r.smirnov86@gmail.com>
2022-01-28 15:35:48 +0600

    добавляет стили для страницы выполненых кейсов

--- gulpfile.js
+++ gulpfile.js
@@ -110,8 +110,7 @@ const copy = (done) => {
   return gulp.src([
     "source/fonts/*.{woff2,woff}",
     "source/*.ico",
-    "source/img/**/*.{jpg,png,svg}",
-    "source/*.*"
+    "source/img/**/*.{jpg,png,svg}"
   ],
   {
     base: "source"
