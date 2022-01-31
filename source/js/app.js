5ae10a234f1717cb2b47f8770c687903532e22de
smirnovrm <r.smirnov86@gmail.com>
2022-01-31 21:18:08 +0600

    добавляет стили для мобильной версии страницы новостей

--- source/js/app.js
+++ source/js/app.js
@@ -6,37 +6,41 @@ window.addEventListener('DOMContentLoaded', () => {
     mainMenu.classList.toggle('main-menu__list--opened')
   });
 
+  try {
 
-  const tabs = document.querySelectorAll('.tabs__control');
-  const contents = document.querySelectorAll('.tabs__content');
+    const tabs = document.querySelectorAll('.tabs__control');
+    const contents = document.querySelectorAll('.tabs__content');
 
-  tabs.forEach((tab, index, parent) => {
-    tab.addEventListener('click', (e) => {
-      e.preventDefault();
-      parent.forEach(item => {
-        item.classList.remove('tabs__control--active');
+    tabs.forEach((tab, index, parent) => {
+      tab.addEventListener('click', (e) => {
+        e.preventDefault();
+        parent.forEach(item => {
+          item.classList.remove('tabs__control--active');
+        });
+        contents.forEach(item => {
+          item.classList.remove('tabs__content--active');
+        });
+        tab.classList.add('tabs__control--active');
+        contents[index].classList.add('tabs__content--active');
       });
-      contents.forEach(item => {
-        item.classList.remove('tabs__content--active');
-      });
-      tab.classList.add('tabs__control--active');
-      contents[index].classList.add('tabs__content--active');
     });
-  });
 
-  const modalButton = document.querySelector('#modal');
-  const closeModalButton = document.querySelector('#close-modal');
-  const modal = document.querySelector('.modal');
+    const modalButton = document.querySelector('#modal');
+    const closeModalButton = document.querySelector('#close-modal');
+    const modal = document.querySelector('.modal');
 
-  modalButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.remove('modal--hide');
-    document.body.style.overflow = 'hidden';
-  });
+    modalButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      modal.classList.remove('modal--hide');
+      document.body.style.overflow = 'hidden';
+    });
 
-  closeModalButton.addEventListener('click', () => {
-    modal.classList.add('modal--hide');
-    document.body.style.overflow = '';
-  });
+    closeModalButton.addEventListener('click', () => {
+      modal.classList.add('modal--hide');
+      document.body.style.overflow = '';
+    });
+  }catch (error) {
+    // console.log(error.text);
+  }
 
 });
